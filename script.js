3f8ab697627975d3ed82f0707e6935f9ab4ee4e9
Joorb <joorb411@gmail.com>
2025-09-25 11:33:01 -0300

    Ainda resolvendo os problemas da barra de navegação do mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+// Selecionando o botão pelo ID
+const botao = document.querySelector('.lblcheck');
+
+botao.addEventListener('click', function() {
+     botao.classList.toggle('lblcheckchecked');
+});
